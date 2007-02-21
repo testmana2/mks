2084b16845bb969e2d3f07891ad774c4f30ef2a3
pasnox <pasnox@eb2ae826-2224-0410-8894-813e3fd817a8>
2007-02-21 22:11:04 +0000

    Add AStyle plugin to allow of code reindent/reformat

---
+++ v2/trunk/2006.2.0.0/plugins/workspace/AStyle/src/AStylePlugin.h
@@ -0,0 +1,28 @@
+#ifndef ASTYLEPLUGIN_H
+#define ASTYLEPLUGIN_H
+//
+#include "WorkspacePlugin.h"
+//
+class UISettingsAStyle;
+class QAction;
+//
+class AStylePlugin : public WorkspacePlugin
+{
+	Q_OBJECT
+	Q_INTERFACES( BasePlugin WorkspacePlugin )
+	//
+public:
+	virtual void initialize( Workspace* );
+	virtual QWidget* settingsWidget();
+	virtual bool install();
+	virtual bool uninstall();
+	//
+private:
+	QAction* aAStyle;
+	//
+private slots:
+	void applyFormatter();
+	//
+};
+//
+#endif // ASTYLEPLUGIN_H
