12f26210f85cddde1dedd0c3e51ea6b1d1e77981
hlamer <hlamer@eb2ae826-2224-0410-8894-813e3fd817a8>
2007-06-13 22:42:14 +0000

    Finished ActionManager

--- v2/branches/dev/2006.2.0.0/monkey/src/actionmanager/KeySequenceInput.h
+++ v2/branches/dev/2006.2.0.0/monkey/src/actionmanager/KeySequenceInput.h
@@ -1,24 +1,37 @@
 #ifndef _KEY_SEQUENCE_INPUT
 #define _KEY_SEQUENCE_INPUT
-#include <QLineEdit>
 
-#include <QWidget>
-#include <QKeyEvent>
 #include <QLineEdit>
+#include <QKeyEvent>
+/*
+Class may be used for defining key sequences. It's lools as QLineEdit,
+but when user pressing some key/combination of keys, 
+widget will display it in the fit of key sequence
+See ActionManager.h for more comments
+
+Author - Kopats Andrei. 
+hlamer at tut dot by
+Writed for the Monkey Developer Studio project
+(www.monkeystudio.org)
+Date: 14/06/2007
+License: GPL
+*/
 
 class KeySequenceInput : public QLineEdit
 {
+
 private:
 	bool controlPressed;
 	bool altPressed;
 	bool winPressed;
 	bool shiftPressed;
-
+	
 public:
-	KeySequenceInput (QWidget*);
+	KeySequenceInput ( QWidget* );
 	
 protected:
-	//maybe possible just overload QObject::event (), but there is some problems	
-	bool eventFilter(QObject *obj, QEvent *event);
+	//maybe possible just overload QObject::event (), but there is some problems
+	bool eventFilter( QObject *obj, QEvent *event );
 };
 #endif //_KEY_SEQUENCE_INPUT
+
--- v2/branches/dev/2006.2.0.0/monkey/src/actionmanager/MonkeyAction.cpp
+++ v2/branches/dev/2006.2.0.0/monkey/src/actionmanager/MonkeyAction.cpp
@@ -2,21 +2,11 @@
 #include "MonkeyActGroup.h"
 #include "ActionManager.h"
 
-MonkeyAction::MonkeyAction ( MonkeyActGroup * parent, const QString & text,const QString name,  const QKeySequence & defaultShortcut ): QAction (parent)
+MonkeyAction::MonkeyAction ( MonkeyActGroup * parent, const QString & text, const QString name, const QKeySequence & defaultShortcut ) : QAction ( parent )
 {
-	
-	setObjectName (name);
-	setShortcutContext( Qt::ApplicationShortcut );
-    setShortcut ( defaultShortcut);
- 	setShortcut ( ActionManager::getShortCut ( parent->objectName(), name, defaultShortcut) );
-    setText (text);
-    parent->addAction ( this);
-	setProperty ("Default shortcut", QVariant (defaultShortcut.toString()));
-	
-}
-
-MonkeyAction::~MonkeyAction ()
-{
-	MonkeyActGroup* group = (MonkeyActGroup*)actionGroup ();
-	group->removeAction ( this);
+	setObjectName ( name );
+	setShortcut ( ActionManager::getShortCut ( parent->objectName(), name, defaultShortcut ) );
+	setText ( text );
+	parent->addAction ( this );
+	setProperty ( "Default shortcut", QVariant ( defaultShortcut.toString() ) );
 }
