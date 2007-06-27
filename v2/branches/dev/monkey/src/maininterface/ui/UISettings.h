0212f702facfeed7da83e951be3d0d4b5e6ece84
pasnox <pasnox@eb2ae826-2224-0410-8894-813e3fd817a8>
2007-06-27 21:10:44 +0000

    add support for templates and word completion shortcut in UISettings

--- v2/branches/dev/monkey/src/maininterface/ui/UISettings.h
+++ v2/branches/dev/monkey/src/maininterface/ui/UISettings.h
@@ -35,17 +35,20 @@ public slots:
 private slots:
 	void on_twMenu_itemSelectionChanged();
 	void on_tbDefaultProjectsDirectory_clicked();
+	void on_tbTemplatesPath_clicked();
+	void on_pbAddTemplateType_clicked();
+	void on_pbEditTemplateType_clicked();
 	void cbAPIsLanguages_beforeChanged( int );
 	void on_cbAPIsLanguages_currentIndexChanged( int );
 	void on_pbAPIsDelete_clicked();
 	void on_pbAPIsAdd_clicked();
 	void on_pbAPIsBrowse_clicked();
 	void tbColours_clicked();
-	void on_twHighlighterAssociations_itemClicked( QTreeWidgetItem*, int );
+	void on_twHighlighterAssociations_itemSelectionChanged();
 	void on_pbHighlighterAssociationAddChange_clicked();
 	void on_pbHighlighterAssociationDelete_clicked();
 	void on_cbSyntaxHighlightingLexerLanguage_currentIndexChanged( const QString& );
-	void on_lwSyntaxHighlightingStyleElements_itemClicked( QListWidgetItem* );
+	void on_lwSyntaxHighlightingStyleElements_itemSelectionChanged();
 	void on_pbSyntaxHighlightingForegroundColour_clicked();
 	void on_pbSyntaxHighlightingBackgroundColour_clicked();
 	void on_pbSyntaxHighlightingFont_clicked();
@@ -55,6 +58,10 @@ private slots:
 	void on_pbSyntaxHighlightingAllFonts_clicked();
 	void on_pbStyleLineNumbersFont_clicked();
 	void on_pbStyleMonospacedFont_clicked();
+	void on_twAbbreviations_itemSelectionChanged();
+	void on_pbAddAbbreviation_clicked();
+	void on_pbRemoveAbbreviation_clicked();
+	void on_teAbbreviation_textChanged();
 
 };
 
