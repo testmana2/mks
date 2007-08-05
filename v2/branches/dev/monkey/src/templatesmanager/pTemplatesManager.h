06e78797a9d5dccb151de815eab09c380ff169ac
pasnox <pasnox@eb2ae826-2224-0410-8894-813e3fd817a8>
2007-08-05 09:24:22 +0000

    * new templates wizard/manager fully working avaialble with c++ class / files / projects * General code update and improvements

--- v2/branches/dev/monkey/src/templatesmanager/pTemplatesManager.h
+++ v2/branches/dev/monkey/src/templatesmanager/pTemplatesManager.h
@@ -14,16 +14,44 @@
 
 #include <QApplication>
 #include <QStringList>
+#include <QComboBox>
 
 class UITemplatesWizard;
 class pTemplatePreviewer;
 
 struct Q_MONKEY_EXPORT pTemplate
 {
-	enum TemplateType { ttUnknow = 0, ttFiles, ttProjects, ttExtras };
+	enum TemplateType { ttUnknow = 0, ttFiles, ttProjects, ttExtras, ttEnd };
 	pTemplate() {}
 	pTemplate( const QString& l, TemplateType t, const QString& n, const QString& d, const QString& i, const QStringList& f )
 		: Language( l ), Type( t ), Name( n ), Description( d ), Icon( i ), FileNames( f ) {}
+
+	static void fillComboBox( QComboBox* cb )
+	{
+		// if no cb, cancel
+		if ( !cb )
+			return;
+		// clear combo
+		cb->clear();
+		// add entries
+		for ( int i = ttUnknow +1; i < pTemplate::ttEnd; i++ )
+			cb->addItem( stringForType( (pTemplate::TemplateType)i ), i );
+	}
+
+	static QString stringForType( pTemplate::TemplateType t )
+	{
+		switch( t )
+		{
+			case ttFiles:
+				return QObject::tr( "Files" );
+			case ttExtras:
+				return QObject::tr( "Extras" );
+			case ttProjects:
+				return QObject::tr( "Projects" );
+			default:
+				return QString();
+		}
+	}
 
 	QString Language;
 	TemplateType Type;
