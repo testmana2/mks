5b9592f6cad70481fe58d0f0ed80dbfbdc7d158b
hlamer <hlamer@eb2ae826-2224-0410-8894-813e3fd817a8>
2007-09-23 16:54:00 +0000

    NoProject ready (just a one visible bug), now need to make some changes in the ConsoleManager system

--- v2/branches/noproject2/trunk/plugins/project/NoProject/src/NoProjectProjectItem.cpp
+++ v2/branches/noproject2/trunk/plugins/project/NoProject/src/NoProjectProjectItem.cpp
@@ -3,7 +3,6 @@
 
 #include "NoProjectProjectItem.h"
 #include "ProjectPlugin.h"
-#include "UIMain.h"
 #include "UINoProjectProjectSettings.h"
 #include "pCommand.h"
 #include "pConsoleManager.h"
@@ -19,14 +18,19 @@ NoProjectProjectItem::NoProjectProjectItem ()
 	setFilePath ("toto.noproject");
 	pMenuBar* mb = pMenuBar::instance();
 	targets.append ( (Target){"Build","",mb->action("mBuild/mBuild/aCurrent")});
+	targets.append ( (Target){"Rebuild","",mb->action("mBuild/mRebuild/aCurrent")});
 	targets.append ( (Target){"Clean","",mb->action("mBuild/mClean/aCurrent")});
 	targets.append ( (Target){"Distclean","",mb->action("mBuild/mDistClean/aCurrent")});
 	targets.append ( (Target){"Execute","",mb->action("mBuild/aExecute")});
+	targets.append ( (Target){"Build & execute","",mb->action("mBuild/aBuildExecute")});
 	editSettings();
 }
 
 NoProjectProjectItem::~NoProjectProjectItem ()
 {
+	removeSelfFromMenu ();
+	qDebug ()<<"oops";
+	model()->removeRow( row(), index().parent() ); // just a little segfault
 }
 
 void NoProjectProjectItem::editSettings()
@@ -63,25 +67,33 @@ void NoProjectProjectItem::makeTarget (int targetIndex)
 void NoProjectProjectItem::removeSelfFromMenu (QMenu* menu)
 {
 	foreach ( Target t, targets)
-		disconnect ( t.action, SIGNAL (triggered()), this, SLOT (buildMenuTriggered()));
+		if (t.action)
+			disconnect ( t.action, SIGNAL (triggered()), this, SLOT (buildMenuTriggered()));
+	for ( int i = ProjectItem::aLast; i < targets.size(); i++)
+	{
+		if (targets[i].action)
+		{
+			delete targets[i].action;  // !!! I can not see way to delete action from menu, so, deleting it and cleating again
+			targets[i].action = NULL;
+		}
+	}
 }
 
 void NoProjectProjectItem::addSelfToMenu (QMenu* menu)
 {
 	if ( !menu)
 		menu = pMenuBar::instance()->menu( "mBuild" );
+	menu->setEnabled (true);	
+	for ( int i = ProjectItem::aLast; i < targets.size(); i++)
+	{
+		targets[i].action = pMenuBar::instance()->action(QString("mBuild/aCustom%1").arg(i),targets[i].text);  // !!! I can not see way to delete action from menu, so, deleting it and creating again
+	}
 	foreach ( Target t, targets)
+	{
 		connect ( t.action, SIGNAL (triggered()), this, SLOT (buildMenuTriggered()));
-}
-
-void NoProjectProjectItem::updateBuildMenu (QMenu* menu)
-{
-	if ( !menu)
-		menu = pMenuBar::instance()->menu( "mBuild" );
-	menu->setEnabled (true);
-	UIMain* main = UIMain::instance();
-	foreach ( Target t, targets)
 		t.action->setEnabled ( !t.command.isEmpty() );
+		t.action->setText (t.text);
+	}
 }
 
 ProjectPlugin* NoProjectProjectItem::getParentPlugin ()
