60a53b95ae8a07e98bf1059e631777d76e1891c8
pasnox <pasnox@eb2ae826-2224-0410-8894-813e3fd817a8>
2008-01-04 22:38:19 +0000

    added missing files

---
+++ v2/branches/dev/fresh/widgets/pDockWidget.h
@@ -0,0 +1,25 @@
+#ifndef PDOCKWIDGET_H
+#define PDOCKWIDGET_H
+
+#include "MonkeyExport.h"
+
+#include <QDockWidget>
+
+class Q_MONKEY_EXPORT pDockWidget : public QDockWidget
+{
+	Q_OBJECT
+	
+public:
+	pDockWidget( const QString&, QWidget* = 0, Qt::WindowFlags = 0 );
+	pDockWidget( QWidget* = 0, Qt::WindowFlags = 0 );
+	QSize sizeHint() const;
+
+public slots:    void setVisible( bool );
+
+protected:
+	QSize contentsSize() const;
+	QSize mSize;
+
+};
+
+#endif // PDOCKWIDGET_H
