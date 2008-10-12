dbbf9ab096890a38d972ae0163d2031cee50dc4f
pasnox <pasnox@eb2ae826-2224-0410-8894-813e3fd817a8>
2008-10-12 21:43:34 +0000

    Remove no longer needed members due to introduction of new item type ( File & Path ). Add some comment

--- v2/branches/xupmanager/core/XUPProjectItem.h
+++ v2/branches/xupmanager/core/XUPProjectItem.h
@@ -37,11 +37,6 @@ public:
 	// return the parent project for include project ( recursive parent project for include project, else return project itself )
 	XUPProjectItem* rootIncludeProject() const;
 	
-	// return true if item is a variable wich values are files
-	bool isFileBased( XUPItem* item ) const;
-	// return true if item is a variable wich values are paths
-	bool isPathBased( XUPItem* item ) const;
-	
 	// return icon filename for item
 	QString iconFileName( XUPItem* item ) const;
 	// return the project icons path
@@ -49,8 +44,6 @@ public:
 	
 	// return the display text of a project variable name
 	QString variableDisplayText( const QString& variableName ) const;
-	// return the display text of a project value
-	QString valueDisplayText( XUPItem* valueItem ) const;
 	
 	// return the display text for the project item
 	QString itemDisplayText( XUPItem* item );
@@ -61,6 +54,7 @@ public:
 	virtual QList<XUPItem*> getVariables( const XUPItem* root, const QString& variableName, const XUPItem* callerItem = 0 ) const;
 	// return the result of interpreting the variable variableName until caller item if defined, or until complete tree is scaned or default value if variable is empty
 	virtual QString interpretVariable( const QString& variableName, const XUPItem* callerItem = 0, const QString& defaultValue = QString::null ) const;
+	// return the result of interpreting the caller attribute
 	virtual QString interpretValue( XUPItem* callerItem, const QString& attribute ) const;
 	
 	// return the project type id
