1503929d7f08d83120423946947760239c81a70e
pasnox <pasnox@eb2ae826-2224-0410-8894-813e3fd817a8>
2008-10-13 23:15:00 +0000

    Add first basic filtering model, ugly and bugguy

--- v2/branches/xupmanager/core/XUPFilteredProjectModel.cpp
+++ v2/branches/xupmanager/core/XUPFilteredProjectModel.cpp
@@ -1,10 +1,12 @@
 #include "XUPFilteredProjectModel.h"
+#include "XUPProjectItem.h"
 
 #include <QDebug>
 
-XUPFilteredProjectModel::XUPFilteredProjectModel( XUPProjectModel* sourceModel )
-	: QAbstractItemModel( sourceModel )
+XUPFilteredProjectModel::XUPFilteredProjectModel( QObject* parent, XUPProjectModel* sourceModel )
+	: QAbstractItemModel( parent )
 {
+	mSourceModel = 0;
 	setSourceModel( sourceModel );
 }
 
@@ -14,6 +16,11 @@ XUPFilteredProjectModel::~XUPFilteredProjectModel()
 
 QModelIndex XUPFilteredProjectModel::index( int row, int column, const QModelIndex& parent ) const
 {
+	if ( mSourceModel && mSourceModel->mRootProject && parent == QModelIndex() && column < columnCount() && row < rowCount() )
+	{
+		return createIndex( row, column, mItems[ mSourceModel->mRootProject ].at( row ) );
+	}
+	
 	/*
 	if ( !hasIndex( row, column, parent ) )
 		return QModelIndex();
@@ -48,6 +55,8 @@ QModelIndex XUPFilteredProjectModel::parent( const QModelIndex& index ) const
 
 int XUPFilteredProjectModel::rowCount( const QModelIndex& parent ) const
 {
+	return mSourceModel && mSourceModel->mRootProject ? mItems[ mSourceModel->mRootProject ].count() : 0;
+
 	//if ( parent.column() > 0 )
 		return 0;
 	/*
@@ -68,33 +77,16 @@ int XUPFilteredProjectModel::rowCount( const QModelIndex& parent ) const
 
 int XUPFilteredProjectModel::columnCount( const QModelIndex& parent ) const
 {
-	/*
-	Q_UNUSED( parent );
-	return 1;
-	*/
-	return 0;
+	return mSourceModel ? mSourceModel->columnCount() : 0;
 }
 
 QVariant XUPFilteredProjectModel::headerData( int section, Qt::Orientation orientation, int role ) const
 {
-	/*
-	if ( orientation == Qt::Horizontal && section == 0 )
-	{
-		if ( mRootProject )
-		{
-			if ( role == Qt::DecorationRole )
-				return mRootProject->displayIcon();
-			else if ( role == Qt::DisplayRole )
-				return mRootProject->displayText();
-		}
-	}
-	*/
-	return QVariant();
+	return mSourceModel ? mSourceModel->headerData( section, orientation, role ) : QVariant();
 }
 
 QVariant XUPFilteredProjectModel::data( const QModelIndex& index, int role ) const
 {
-	/*
 	if ( !index.isValid() )
 		return QVariant();
 
@@ -104,11 +96,7 @@ QVariant XUPFilteredProjectModel::data( const QModelIndex& index, int role ) con
 		case Qt::DisplayRole:
 		case Qt::ToolTipRole:
 		{
-			XUPItem* item = static_cast<XUPItem*>( index.internalPointer() );
-
-			QDomNode node = item->node();
-			QStringList attributes;
-			QDomNamedNodeMap attributeMap = node.attributes();
+			XUPItem* item = mItems[ mSourceModel->mRootProject ].at( index.row() );
 			
 			if ( role == Qt::DecorationRole )
 			{
@@ -120,18 +108,13 @@ QVariant XUPFilteredProjectModel::data( const QModelIndex& index, int role ) con
 			}
 			else if ( role == Qt::ToolTipRole )
 			{
-				for ( int i = 0; i < attributeMap.count(); i++ )
-				{
-					QDomNode attribute = attributeMap.item( i );
-					attributes << attribute.nodeName() +"=\"" +attribute.nodeValue() +"\"";
-				}
-				return attributes.join( "\n" );
+				return "Tooltip";
 			}
 		}
 		default:
 			break;
 	}
-	*/
+	
 	return QVariant();
 }
 
@@ -142,9 +125,80 @@ Qt::ItemFlags XUPFilteredProjectModel::flags( const QModelIndex& index ) const
 	return Qt::ItemIsEnabled | Qt::ItemIsSelectable;
 }
 
+XUPItemList XUPFilteredProjectModel::getFilteredVariables( const XUPItem* root ) const
+{
+	XUPItemList variables;
+	XUPProjectItem* rootProject = mSourceModel->mRootProject;
+	
+	for ( int i = 0; i < root->childCount(); i++ )
+	{
+		XUPItem* item = root->child( i );
+		
+		switch ( item->type() )
+		{
+			case XUPItem::Project:
+			{
+				/*
+				XUPItem* pItem = item->parent();
+				if ( pItem->type() == XUPItem::Function && pItem->attribute( "name" ).toLower() == "include" )
+					variables << getFilteredVariables( item, variableName, callerItem );
+				*/
+				break;
+			}
+			case XUPItem::Comment:
+				break;
+			case XUPItem::EmptyLine:
+				break;
+			case XUPItem::Variable:
+			{
+				const QStringList filteredVariables = rootProject->projectInfos()->filteredVariables( rootProject->projectType() );
+				if ( filteredVariables.contains( item->attribute( "name" ) ) )
+					variables << item;
+				break;
+			}
+			case XUPItem::Value:
+				break;
+			case XUPItem::Function:
+			{
+				variables << getFilteredVariables( item );
+				break;
+			}
+			case XUPItem::Scope:
+			{
+				variables << getFilteredVariables( item );
+				break;
+			}
+			default:
+				break;
+		}
+	}
+	
+	return variables;
+}
+
 void XUPFilteredProjectModel::setSourceModel( XUPProjectModel* model )
 {
 	mSourceModel = model;
+	
+	reset();
+	mItems.clear();
+	
+	if ( !mSourceModel )
+		return;
+	
+	beginInsertColumns( QModelIndex(), 0, 0 );
+	endInsertColumns();
+	
+	// populate tree...
+	XUPItemList variables = getFilteredVariables( mSourceModel->mRootProject );
+	mItems[ mSourceModel->mRootProject ] = variables;
+	
+	const int count = variables.count();
+	if ( count > 0 )
+	{
+		beginInsertRows( QModelIndex(), 0, count -1 );
+		endInsertRows();
+	}
 }
 
 XUPProjectModel* XUPFilteredProjectModel::sourceModel() const
