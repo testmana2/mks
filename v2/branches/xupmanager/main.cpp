93770b35e44193903fabc2ada504a8a5e3dc394c
pasnox <pasnox@eb2ae826-2224-0410-8894-813e3fd817a8>
2008-11-03 21:56:01 +0000

    add qsrand initialisation

--- v2/branches/xupmanager/main.cpp
+++ v2/branches/xupmanager/main.cpp
@@ -9,6 +9,8 @@ int main( int argc, char** argv )
 {
 	QApplication app( argc, argv );
 	
+	qsrand( QTime( 0, 0, 0 ).secsTo( QTime::currentTime() ) );
+	
 	// register project type
 	( new XUPProjectItem() )->registerProjectType();
 	( new QMakeProjectItem() )->registerProjectType();
