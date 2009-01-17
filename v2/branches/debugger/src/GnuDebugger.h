180681e1850e587f1763b06918ebeef269ce7d45
hlamer <hlamer@eb2ae826-2224-0410-8894-813e3fd817a8>
2009-01-17 20:38:44 +0000

    Backtrace view funtionality of debugger

--- v2/branches/debugger/src/GnuDebugger.h
+++ v2/branches/debugger/src/GnuDebugger.h
@@ -4,6 +4,7 @@
 #include <QObject>
 #include <QString>
 #include <QList>
+#include <QTimer>
 
 #include "mi_gdb.h"
 
@@ -34,7 +35,6 @@ public:
 	
 	// preparing to execution
 	void prepare_startXterm ();
-	void prepare_loadTarget ();
 	
 	// target execution
 	
@@ -50,12 +50,36 @@ public:
 	
 	// stack
 	void stack_Info ();
-
+	
 protected:
 	mi_h* mHandle;
 	mi_aux_term* mXterm;
-
+	
+	bool mTargetBeenStopped;
+	
+	QTimer mGdbPingTimer;
+	
+	
+	// migdb callbacks
+	static void callbackConsole (const char *, void *);
+	static void callbackTarget(const char *str, void *);
+	static void callbackLog (const char *str, void *);
+	static void callbackToGDB (const char *str, void *);
+	static void callbackFromGDB (const char *str, void *);
+	static void callbackAsync (mi_output * o, void * debuggerInstance);
+	
+	void internalUpdate ();
+	
+	
+protected slots:
+	// touches gdb for make it alive
+	// Without it we haven't callbacks
+	void onGdbTouchTimerTick ();
+	
+	void onStopped();
+	
 signals:
+	void stopped ();
 	void positionChanged (const QString& fileName, int line);
 	
 	void callStackUpdate (const GnuDebugger::CallStack&);
