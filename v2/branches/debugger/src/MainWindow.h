fd2d507816f434a0d7e69780aaebb0e3d668411d
hlamer <hlamer@eb2ae826-2224-0410-8894-813e3fd817a8>
2009-01-18 16:17:45 +0000

    Debugger is able to load target, control it's execution

--- v2/branches/debugger/src/MainWindow.h
+++ v2/branches/debugger/src/MainWindow.h
@@ -1,7 +1,11 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include "GnuDebugger.h"
+
 #include "ui_MainWindow.h"
+
+class QLabel;
 
 class MainWindow : public QMainWindow, public Ui::MainWindow
 {
@@ -10,6 +14,27 @@ class MainWindow : public QMainWindow, public Ui::MainWindow
 public:
 	MainWindow( QWidget* parent = 0 );
 	virtual ~MainWindow();
+	
+protected:
+	QLabel* mStatusLabel;
+	
+protected slots:
+	void loadTargetTriggered();
+
+public slots:
+	void onDebuggerStateChanged (GnuDebugger::State);
+signals:
+	
+	// api
+	void loadTargetRequested (const QString& fileName);
+	
+	void exec_runTriggered();
+	void exec_continueTriggered();
+	void exec_stepIntoTriggered();
+	void exec_stepOverTriggered();
+	void exec_stepOutTriggered();
+	void exec_pauseTriggered();
+	void exec_killTriggered();
 };
 
 #endif // MAINWINDOW_H
