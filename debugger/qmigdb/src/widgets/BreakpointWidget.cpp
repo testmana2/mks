d8c4ebc9092978b7d046bb4632f0a62d25c5fac7
pasnox <pasnox@eb2ae826-2224-0410-8894-813e3fd817a8>
2009-01-24 23:58:25 +0000

    Add new icons. Add missing files

---
+++ debugger/qmigdb/src/widgets/BreakpointWidget.cpp
@@ -0,0 +1,63 @@
+#include "BreakpointWidget.h"
+
+#include <QDebug>
+
+BreakpointWidget::BreakpointWidget( QGdbDriver* driver, QWidget* parent )
+	: QTreeWidget( parent )
+{
+	setColumnCount( 4 );
+	setHeaderLabels( QStringList( tr( "Enabled" ) ) << tr( "Type" ) << tr( "Filename/Address" ) << tr( "Line" ) );
+	
+	connect( driver, SIGNAL( breakpointAdded( const QGdbDriver::Breakpoint& ) ), this, SLOT( breakpointAdded( const QGdbDriver::Breakpoint& ) ) );
+	connect( driver, SIGNAL( breakpointRemoved( const QGdbDriver::Breakpoint& ) ), this, SLOT( breakpointRemoved( const QGdbDriver::Breakpoint& ) ) );
+	connect( driver, SIGNAL( breakpointUpdated( const QGdbDriver::Breakpoint& ) ), this, SLOT( breakpointUpdated( const QGdbDriver::Breakpoint& ) ) );
+}
+
+BreakpointWidget::~BreakpointWidget()
+{
+}
+
+void BreakpointWidget::breakpointAdded( const QGdbDriver::Breakpoint& breakpoint )
+{
+	QTreeWidgetItem* item = new QTreeWidgetItem( this );
+	updateItem( item, breakpoint );
+}
+
+void BreakpointWidget::breakpointRemoved( const QGdbDriver::Breakpoint& breakpoint )
+{
+	for ( int i = 0; i < topLevelItemCount(); i++ )
+	{
+		QTreeWidgetItem* item = topLevelItem( i );
+		QGdbDriver::Breakpoint bp = item->data( 0, Qt::UserRole ).value<QGdbDriver::Breakpoint>();
+		
+		if ( breakpoint.number == bp.number )
+		{
+			delete item;
+			return;
+		}
+	}
+}
+
+void BreakpointWidget::breakpointUpdated( const QGdbDriver::Breakpoint& breakpoint )
+{
+	for ( int i = 0; i < topLevelItemCount(); i++ )
+	{
+		QTreeWidgetItem* item = topLevelItem( i );
+		QGdbDriver::Breakpoint bp = item->data( 0, Qt::UserRole ).value<QGdbDriver::Breakpoint>();
+		
+		if ( breakpoint.number == bp.number )
+		{
+			updateItem( item, breakpoint );
+			return;
+		}
+	}
+}
+
+void BreakpointWidget::updateItem( QTreeWidgetItem* item, const QGdbDriver::Breakpoint& breakpoint )
+{
+	item->setCheckState( 0, breakpoint.enabled ? Qt::Checked : Qt::Unchecked );
+	item->setText( 1, breakpoint.stringType() );
+	item->setText( 2, breakpoint.textMode() );
+	item->setText( 3, QString::number( breakpoint.line ) );
+	item->setData( 0, Qt::UserRole, QVariant::fromValue( breakpoint ) );
+}
