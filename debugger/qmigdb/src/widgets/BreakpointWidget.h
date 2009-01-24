67b0c6dd03075616fcd87bd87f4651f0ed00f9dc
pasnox <pasnox@eb2ae826-2224-0410-8894-813e3fd817a8>
2009-01-24 23:58:25 +0000

    Add new icons. Add missing files

---
+++ debugger/qmigdb/src/widgets/BreakpointWidget.h
@@ -0,0 +1,25 @@
+#ifndef BREAKPOINTWIDGET_H
+#define BREAKPOINTWIDGET_H
+
+#include <QTreeWidget>
+
+#include "QGdbDriver.h"
+
+class BreakpointWidget : public QTreeWidget
+{
+	Q_OBJECT
+	
+public:
+	BreakpointWidget( QGdbDriver* driver, QWidget* parent = 0 );
+	virtual ~BreakpointWidget();
+
+protected slots:
+	void breakpointAdded( const QGdbDriver::Breakpoint& breakpoint );
+	void breakpointRemoved( const QGdbDriver::Breakpoint& breakpoint );
+	void breakpointUpdated( const QGdbDriver::Breakpoint& breakpoint );
+
+protected:
+	void updateItem( QTreeWidgetItem* item, const QGdbDriver::Breakpoint& breakpoint );
+};
+
+#endif // BREAKPOINTWIDGET_H
