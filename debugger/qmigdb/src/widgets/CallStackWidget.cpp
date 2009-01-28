168f68ed948e02e0d93831fdd6014923c008ed77
pasnox <pasnox@eb2ae826-2224-0410-8894-813e3fd817a8>
2009-01-28 19:27:09 +0000

    Add best management of call stack selected frames

--- debugger/qmigdb/src/widgets/CallStackWidget.cpp
+++ debugger/qmigdb/src/widgets/CallStackWidget.cpp
@@ -3,10 +3,9 @@
 CallStackWidget::CallStackWidget( QGdbDriver* debugger, QWidget* parent )
 	: QTreeWidget( parent )
 {
-	setColumnCount (4);
-	QStringList headers;
-	headers << "Nr" << "Function" << "Location" << "Line number";
-	setHeaderLabels (headers);
+	setColumnCount( 4 );
+	setHeaderLabels( QStringList( tr( "Level" ) ) << tr( "Function" ) << tr( "Location" ) << tr( "Line" ) );
+	
 	connect(this, SIGNAL(itemActivated(QTreeWidgetItem*, int)), this, SLOT(onItemActivated (QTreeWidgetItem*)));
 	
 	// interface with debugger
@@ -22,8 +21,10 @@ void CallStackWidget::update( const QGdbDriver::CallStack& stack, int selectedLe
 	
 	foreach ( const QGdbDriver::Frame& frame, stack )
 	{
-		QTreeWidgetItem* item = new QTreeWidgetItem (this);
-		item->setText (0, QString::number (frame.level));
+		QTreeWidgetItem* item = new QTreeWidgetItem( this );
+		
+		// level
+		item->setText( 0, QString::number( frame.level ) );
 		
 		// function name and arguments
 		QStringList arguments;
@@ -32,22 +33,21 @@ void CallStackWidget::update( const QGdbDriver::CallStack& stack, int selectedLe
 			arguments << QString( "%1 = %2" ).arg( argument.name ).arg( argument.value );
 		}
 		QString argString = arguments.join( ", " ).prepend( "(" ).append( ")" );
-		item->setText (1, frame.function + argString);
+		item->setText( 1, frame.function +argString );
 		
-		// library, file
+		// library, file, line
 		if ( frame.from.isEmpty() )
 		{
-			item->setText(2, QString( "in %1" ).arg( frame.file ));
+			item->setText( 2, tr( "in %1" ).arg( frame.file ) );
+			item->setText( 3, QString::number( frame.line ) );
 		}
 		else
 		{
-			item->setText(2, QString( "from %1:%2" ).arg( frame.from ).arg (frame.line));
+			item->setText( 2, tr( "from %1" ).arg( frame.from ) );
 		}
 		
-		// line
-		item->setText (3, QString::number (frame.line));
-		
-		item->setData(0, Qt::UserRole, QVariant::fromValue( frame ) );
+		// frame datas
+		item->setData( 0, Qt::UserRole, QVariant::fromValue( frame ) );
 		
 		if ( selectedLevel == frame.level )
 		{
@@ -56,8 +56,8 @@ void CallStackWidget::update( const QGdbDriver::CallStack& stack, int selectedLe
 	}
 }
 
-void CallStackWidget::onItemActivated (QTreeWidgetItem* item)
+void CallStackWidget::onItemActivated( QTreeWidgetItem* item )
 {
-	QGdbDriver::Frame frame = item->data(0, Qt::UserRole).value<QGdbDriver::Frame>();
-	emit frameSelected (frame.file, frame.line);
+	QGdbDriver::Frame frame = item->data( 0, Qt::UserRole ).value<QGdbDriver::Frame>();
+	emit frameSelected( frame );
 }
