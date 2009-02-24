5fb6b42522394a9335c5ce02f420809b3cc41d9a
hlamer <hlamer@eb2ae826-2224-0410-8894-813e3fd817a8>
2009-02-24 19:35:20 +0000

    Added missing files

---
+++ debugger/qmigdb/src/widgets/VariablesModel.h
@@ -0,0 +1,34 @@
+#ifndef VARIABLESMODEL_H
+#define VARIABLESMODEL_H
+
+#include <QStandardItem>
+#include <QAbstractItemModel>
+
+#include "QGdbDriver.h"
+
+class VariablesModel : public QAbstractItemModel
+{
+public:
+	VariablesModel (QGdb::Driver* driver);
+	virtual ~VariablesModel ();
+	
+	// QAbstractItemModel standard item set
+	QModelIndex index ( int row, int column, const QModelIndex & parent = QModelIndex() ) const;
+	QModelIndex parent ( const QModelIndex & index ) const;
+	int rowCount ( const QModelIndex & parent = QModelIndex() ) const;
+	int columnCount ( const QModelIndex & parent = QModelIndex() ) const;
+	QVariant data ( const QModelIndex & index, int role = Qt::DisplayRole ) const;
+	
+protected:
+	QStandardItem mArgumentsRoot;
+	QStandardItem mLocalsRoot;
+	QStandardItem mWatchesRoot;
+	
+	QGdb::Driver* mDriver;
+	
+protected slots:
+	void localsUpdated ();
+	void argumentsUpdated ();
+};
+
+#endif // VARIABLESMODEL_H
