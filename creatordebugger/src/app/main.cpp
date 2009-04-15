cbeb758170ed26b46b8c9877854d141612ba9b22
hlamer <hlamer@eb2ae826-2224-0410-8894-813e3fd817a8>
2009-04-15 05:00:06 +0000

    Current progress

--- creatordebugger/src/app/main.cpp
+++ creatordebugger/src/app/main.cpp
@@ -1,12 +1,23 @@
 #include <QApplication>
+#include <QMainWindow>
+#include <QStatusBar>
+#include <QLabel>
 #include <QDebug>
 
 #include <debuggermanager.h>
+#include <debuggerplugin.h>
 
 int main(int argc, char **argv)
 {
 	QApplication app (argc, argv);
 	qDebug () << "create manager";
+#if 0
 	Debugger::Internal::DebuggerManager manager;
+	QMainWindow* mw = manager.mainWindow();
+	mw->statusBar()->addWidget (manager.statusLabel());
+	
+	mw->show();
+#endif
+	Debugger::Internal::DebuggerPlugin plugin;
 	return app.exec();
 }
