7463dc22d8c6fbafd1fbca1def243668573e592d
pasnox <pasnox@eb2ae826-2224-0410-8894-813e3fd817a8>
2009-12-13 12:12:21 +0000

    Update tools.

--- v2/tools/svn_log_to_change_log.php
+++ v2/tools/svn_log_to_change_log.php
@@ -1,31 +1,41 @@
 #!/usr/bin/php
 <?php
-//echo $argv[0]." kool\n";
-
 function usage( $argc, $argv )
 {
 	echo "SvnLog to ChangeLog.\n";
 	echo "\n";
-	echo "Usage:	". $argv[ 0 ] ." path_to_check [output_file_path]\n";
+	echo "Usage:	". $argv[ 0 ] ." [path_to_check] [output_file_path]\n";
 	echo "\n";
-	echo "	path_to_check is the svn working copy path to check for log.\n";
-	echo "	output_file_path is an optionnal file path to write the result to, defaulting to ./ChangeLog\n";
+	echo "	path_to_check is the svn working copy path to check for log, defaulting to ../branches/dev.\n";
+	echo "	output_file_path is an optionnal file path to write the result to, defaulting to ../branches/dev/ChangeLog\n";
 }
 
 function generateChangeLog( $argc, $argv )
 {
-	$path = $argv[ 1 ];
-	$fileName = "./ChangeLog";
+	$path = "../branches/dev";
+	$fileName = "../branches/dev/ChangeLog";
+	$result = -1;
+	
+	if ( $argc > 1 )
+	{
+		$path = $argv[ 1 ];
+	}
 	
 	if ( $argc > 2 )
 	{
 		$fileName = $argv[ 2 ];
 	}
 	
-	system( "svn2cl --include-rev --linelen=80 --output=\"$fileName\" \"$path\"" );
+	system( "svn2cl/svn2cl.sh --include-rev --break-before-msg=2 --output=\"$fileName\" \"$path\"", $result );
+	
+	if ( $result != 0 )
+	{
+		echo "You can try to download the used tool here: http://arthurdejong.org/svn2cl/svn2cl-0.12.tar.gz\n";
+		echo "And extract it here in folder: svn2cl.\n";
+	}
 }
 
-if ( $argc == 1 )
+if ( $argc == 2 && ( $argv[ 1 ] == "-h" || $argv[ 1 ] == "--help" ) )
 {
 	usage( $argc, $argv );
 	exit;
