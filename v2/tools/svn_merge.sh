fd0e196cd7dd07f99bd3eaacabf5786c02867268
pasnox <pasnox@eb2ae826-2224-0410-8894-813e3fd817a8>
2009-12-13 09:57:56 +0000

    Move tools folder as it's not needed to duplicate it.

---
+++ v2/tools/svn_merge.sh
@@ -0,0 +1,25 @@
+rev1=$1
+rev2=$2
+
+usage()
+{
+	echo "MkS Svn Merging Tool."
+	echo ""
+	echo "Usage:	$0 revision1 [revision2]"
+	echo ""
+	echo "	revision1 is the revision to compare from."
+	echo "	revision2 is the revision to compare to, defaulting to HEAD."
+}
+
+if [ -z "$rev1" ]; then
+	usage
+	exit
+fi
+
+if [ -z "$rev2" ]; then
+	rev2="HEAD";
+fi
+
+echo "Merging using first revision at $rev1 and second at $rev2 from branches/dev to trunk..."
+
+svn merge -r $rev1:$rev2 branches/dev trunk
