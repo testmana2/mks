fe69994146d7fb881ec434455d3d1aa7894ff65f
hlamer <hlamer@eb2ae826-2224-0410-8894-813e3fd817a8>
2010-08-06 13:07:46 +0000

    CLIToolPlugin, InterpreterPlugin, and BuilderPlugin merged to one CLIToolPlugin. partial commit

---
+++ v2/branches/newxup/monkey/src/pluginsmanager/CLIToolPlugin.cpp
@@ -0,0 +1,61 @@
+/****************************************************************************
+**
+** 		Created using Monkey Studio v1.8.1.0
+** Authors   : Filipe AZEVEDO aka Nox P@sNox <pasnox@gmail.com>, Andrei KOPATS aka hlamer <hlamer@tut.by>
+** Project   : Monkey Studio IDE
+** FileName  : CLIToolPlugin.cpp
+** Date      : 2009-12-09T00:37:00
+** License   : GPL
+** Comment   : 
+** Home Page : http://www.monkeystudio.org
+**
+**
+****************************************************************************/
+#include "CLIToolPlugin.h"
+#include "ui/UIInterpreterSettings.h"
+
+CLIToolPlugin::CLIToolPlugin()
+	: BasePlugin(), CLIToolInterface( this )
+{
+}
+
+pCommand CLIToolPlugin::command() const
+{
+	// get settings object
+	pSettings* settings = MonkeyCore::settings();
+	pCommand cmd;
+	
+	cmd.setText( settings->value( settingsKey( "command/Text" ) ).toString() );
+	cmd.setCommand( settings->value( settingsKey( "command/Command" ) ).toString() );
+	cmd.setArguments( settings->value( settingsKey( "command/Arguments" ) ).toString() );
+	cmd.setWorkingDirectory( settings->value( settingsKey( "command/WorkingDirectory" ) ).toString() );
+	cmd.setParsers( settings->value( settingsKey( "command/Parsers" ) ).toStringList() );
+	cmd.setTryAllParsers( settings->value( settingsKey( "command/TryAll" ), false ).toBool() );
+	cmd.setSkipOnError( settings->value( settingsKey( "command/SkipOnError" ), false ).toBool() );
+	
+	// if no user commands get global ones
+	if ( !cmd.isValid() )
+	{
+		cmd = defaultCommand();
+	}
+	
+	return cmd;
+}
+
+void CLIToolPlugin::setCommand( const pCommand& cmd )
+{
+	pSettings* settings = MonkeyCore::settings();
+	
+	settings->setValue( settingsKey( "command/Text" ), cmd.text() );
+	settings->setValue( settingsKey( "command/Command" ), cmd.command() );
+	settings->setValue( settingsKey( "command/Arguments" ), cmd.arguments() );
+	settings->setValue( settingsKey( "command/WorkingDirectory" ), cmd.workingDirectory() );
+	settings->setValue( settingsKey( "command/Parsers" ), cmd.parsers() );
+	settings->setValue( settingsKey( "command/TryAll" ), cmd.tryAllParsers() );
+	settings->setValue( settingsKey( "command/SkipOnError" ), cmd.skipOnError() );
+}
+
+QWidget* CLIToolPlugin::settingsWidget()
+{
+	return new UIInterpreterSettings( this, QApplication::activeWindow() );
+}
