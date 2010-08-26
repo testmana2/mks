f02c921141d6d0963175354cf4c44cf9bbb8c0e2
hlamer <hlamer@eb2ae826-2224-0410-8894-813e3fd817a8>
2010-08-26 08:09:31 +0000

    Makefile project support

---
+++ v2/branches/newxup/plugins/xup/MakefileProject/MakefileProjectItem.cpp
@@ -0,0 +1,111 @@
+#include "MakefileProjectItem.h"
+#include "PluginsManager.h"
+#include "XUPPlugin.h"
+
+#include <ProjectTypesIndex.h>
+#include <pMonkeyStudio.h>
+#include <pFileManager.h>
+#include <CLIToolPlugin.h>
+#include <UIMain.h>
+
+#include <QApplication>
+#include <QTextCodec>
+#include <QMessageBox>
+#include <QFile>
+#include <QDir>
+#include <QFileInfo>
+
+#include <QDebug>
+
+MakefileProjectItem::MakefileProjectItem()
+{
+	mDomElement = mDocument.createElement("project");
+}
+
+QString MakefileProjectItem::projectType() const
+{
+	return PROJECT_TYPE_STRING;
+}
+
+XUPProjectItem* MakefileProjectItem::newProject() const
+{
+	return new MakefileProjectItem();
+}
+
+QStringList MakefileProjectItem::autoActivatePlugins() const
+{
+	return QStringList("GNUMake");
+}
+
+void MakefileProjectItem::installCommands()
+{
+	QStringList targets;
+	QFile file(fileName());
+	QRegExp targetRex("^([\\w\\-\\d]+):.*");
+	while (! file.atEnd())
+	{
+		QString line = file.readLine();
+		int pos = targetRex.indexIn(line);
+		if (-1 != pos)
+			targets << targetRex.cap(1);
+	}
+	CLIToolPlugin* make = MonkeyCore::pluginsManager()->plugin<CLIToolPlugin*>( PluginsManager::stAll, "GNUMake" );
+	if (! make)
+	{
+		QMessageBox::critical(MonkeyCore::mainWindow(), tr("Makefile Project"),
+							  tr("Can't build Makefile projects. GNUMake plugin not enabled"));
+		return;
+	}
+	
+	pCommand baseCmd = make->command();
+	
+	baseCmd.setProject( this );
+	baseCmd.setSkipOnError( false );
+	baseCmd.setTryAllParsers( true );
+	
+	foreach(QString target, targets)
+	{
+		baseCmd.setText( target );
+		baseCmd.setArguments( target );
+		addCommand( baseCmd, "mBuilder" );
+	}
+}
+
+void MakefileProjectItem::addFiles( const QStringList&, XUPItem*)
+{
+	QMessageBox::information (MonkeyCore::mainWindow(), 
+							  tr("Makefile project"),
+							  tr("Adding files to the Makefile project does not make sense. "
+							    "Press 'Edit current project...' for edit your Makefile"));
+}
+
+void MakefileProjectItem::removeItem( XUPItem* )
+{
+	Q_ASSERT(0);
+}
+
+bool MakefileProjectItem::open( const QString& fileName, const QString& codec )
+{
+	mFileName = fileName;
+	mCodec = codec;
+	QString parentDirName = QFileInfo(QFileInfo(fileName).path()).fileName();
+	setAttribute("name",
+				 parentDirName + "/" + QFileInfo(fileName).fileName()); // "directoryname/Makefile"
+	return true;
+}
+
+bool MakefileProjectItem::edit()
+{
+	MonkeyCore::fileManager()->openFile(fileName(), codec());
+	return false; /* not saved */
+}
+
+QString MakefileProjectItem::iconsPath() const
+{
+	return ":/pyqtitems";
+}
+
+StringStringListList MakefileProjectItem::sourceFileNamePatterns() const
+{
+	return StringStringListList();
+}
