2bc6973f1c2835537ba0cb4ed24b47b59165e58e
pasnox <pasnox@eb2ae826-2224-0410-8894-813e3fd817a8>
2011-08-11 15:04:14 +0000

    Rework CommandsEditor widget and now use a model for managing things. Less code -- better/easier code.

---
+++ v2/branches/newxup/monkey/src/xupmanager/gui/CommandsEditorModel.cpp
@@ -0,0 +1,287 @@
+#include "CommandsEditorModel.h"
+#include "pCommand.h"
+
+#include <pMenuBar.h>
+
+#include <QApplication>
+#include <QDebug>
+
+#define CommandsEditorModelColumnCount 1
+
+CommandsEditorModel::CommandsEditorModel( QObject* parent )
+	: QAbstractItemModel( parent )
+{
+	mMenuBar = 0;
+}
+
+CommandsEditorModel::~CommandsEditorModel()
+{
+}
+
+int CommandsEditorModel::columnCount( const QModelIndex& parent ) const
+{
+	Q_UNUSED( parent );
+	return CommandsEditorModelColumnCount;
+}
+
+QVariant CommandsEditorModel::data( const QModelIndex& index, int role ) const
+{
+	const bool isMenu = index.parent() == QModelIndex();
+	const QString menu = this->menu( index );
+	const pCommand command = this->command( index );
+	const bool enabled = command.userData() != Qt::Checked;
+	
+	switch ( role ) {
+		case Qt::CheckStateRole:
+			return isMenu ? QVariant() : ( command.userData().isNull() ? Qt::Unchecked : command.userData().toInt() );
+		case Qt::DecorationRole: {
+			const QIcon icon = isMenu ? ( mMenuBar && !menu.isEmpty() ? mMenuBar->menu( menu )->icon() : QIcon() ) : QIcon();
+			return icon.isNull() ? QVariant() : icon;
+		}
+		case Qt::DisplayRole:
+		case Qt::ToolTipRole:
+			return isMenu ? ( mMenuBar && !menu.isEmpty() ? mMenuBar->menu( menu )->title() : QVariant() ) : command.text();
+		case Qt::ForegroundRole:
+			return !isMenu && !enabled ? QApplication::palette().brush( QPalette::Disabled, QPalette::WindowText ) : QVariant();
+		case Qt::FontRole: {
+			QFont font;
+			font.setStrikeOut( !isMenu && !enabled );
+			return font;
+		}
+		case Qt::EditRole:
+			return QVariant::fromValue( command );
+	}
+	
+	return QVariant();
+}
+
+QModelIndex CommandsEditorModel::index( int row, int column, const QModelIndex& parent ) const
+{
+	if ( row < 0 || column < 0 || column >= CommandsEditorModelColumnCount ) {
+		return QModelIndex();
+	}
+	
+	if ( parent == QModelIndex() ) {
+		return row < mCommands.count() ? createIndex( row, column, -1 ) : QModelIndex();
+	}
+	
+	const QString menu = this->menu( parent );
+	
+	if ( menu.isEmpty() || row >= mCommands.value( menu ).count() ) {
+		return QModelIndex();
+	}
+	
+	return createIndex( row, column, mCommands.keys().indexOf( menu ) );
+}
+
+QModelIndex CommandsEditorModel::parent( const QModelIndex& index ) const
+{
+	return index.internalId() == -1 ? QModelIndex() : this->index( mCommands.keys().value( index.internalId() ) );
+}
+
+int	CommandsEditorModel::rowCount( const QModelIndex& parent ) const
+{
+	return parent == QModelIndex() ? mCommands.count() : mCommands.value( menu( parent ) ).count();
+}
+
+Qt::ItemFlags CommandsEditorModel::flags( const QModelIndex& index ) const
+{
+	Qt::ItemFlags flags = QAbstractItemModel::flags( index );
+	
+	if ( index.isValid() && index.parent().isValid() ) {
+		flags |= Qt::ItemIsUserCheckable;
+	}
+	
+	return flags;
+}
+
+bool CommandsEditorModel::setData( const QModelIndex& index, const QVariant& value, int role )
+{
+	if ( !index.isValid() || index.parent() == QModelIndex() ) {
+		return false;
+	}
+	
+	const QString menu = this->menu( index.parent() );
+	
+	if ( menu.isEmpty() ) {
+		return false;
+	}
+	
+	switch ( role ) {
+		case Qt::CheckStateRole:
+			mCommands[ menu ][ index.row() ].setUserData( value );
+			break;
+		case Qt::EditRole:
+			mCommands[ menu ][ index.row() ] = value.value<pCommand>();
+			break;
+		default:
+			return false;
+	}
+	
+	emit dataChanged( index, index );
+	return true;
+}
+
+QModelIndex CommandsEditorModel::index( const QString& menu ) const
+{
+	if ( !mCommands.contains( menu ) ) {
+		return QModelIndex();
+	}
+	
+	return index( mCommands.keys().indexOf( menu ), 0 );
+}
+
+QModelIndex CommandsEditorModel::index( const pCommand& command, const QString& menu ) const
+{
+	const int row = mCommands.value( menu ).indexOf( command );
+	return row != -1 ? index( row, 0, index( menu ) ) : QModelIndex();
+}
+
+QString CommandsEditorModel::menu( const QModelIndex& index ) const
+{
+	return index.parent() == QModelIndex() ? mCommands.keys().value( index.row() ) : QString::null;
+}
+
+pCommand CommandsEditorModel::command( const QModelIndex& index ) const
+{
+	const QString menu = this->menu( index.parent() );
+	const int count = mCommands.value( menu ).count();
+	return menu.isEmpty() || index.row() >= count ? pCommand() : mCommands[ menu ][ index.row() ];
+}
+
+bool CommandsEditorModel::isEmpty() const
+{
+	foreach ( const QString& menu, mCommands.keys() ) {
+		if ( !mCommands[ menu ].isEmpty() ) {
+			return false;
+		}
+	}
+	
+	return true;
+}
+
+void CommandsEditorModel::clear()
+{
+	if ( isEmpty() ) {
+		return;
+	}
+	
+	emit layoutAboutToBeChanged();
+	
+	const QModelIndexList oldIndexes = persistentIndexList();
+	QModelIndexList newIndexes;
+	
+	foreach ( const QModelIndex& index, oldIndexes ) {
+		if ( index.parent() != QModelIndex() ) {
+			newIndexes << QModelIndex();
+		}
+		else {
+			newIndexes << index;
+		}
+	}
+	
+	foreach ( const QString& menu, mCommands.keys() ) {
+		mCommands[ menu ].clear();
+	}
+	
+	changePersistentIndexList( oldIndexes, newIndexes );
+	
+	emit layoutChanged();
+}
+	
+void CommandsEditorModel::setCommands( const MenuCommandListMap& commands, pMenuBar* menuBar )
+{
+	clear();
+	
+	mMenuBar = menuBar;
+	
+	if ( mCommands.isEmpty() ) {
+		const QStringList menus = mMenuBar ? mMenuBar->rootMenusPath() : QStringList();
+		const int count = menus.count();
+		
+		if ( count > 0 ) {
+			beginInsertRows( QModelIndex(), 0, count -1 );
+			foreach ( const QString& menu, menus ) {
+				mCommands[ menu ] = pCommandList();
+			}
+			endInsertRows();
+		}
+	}
+	else {
+		emit dataChanged( index( 0, 0 ), index( rowCount() -1, columnCount() -1 ) );
+	}
+	
+	foreach ( const QString& menu, commands.keys() ) {
+		addCommands( menu, commands[ menu ] );
+	}
+}
+
+MenuCommandListMap CommandsEditorModel::commands() const
+{
+	return mCommands;
+}
+
+QModelIndex CommandsEditorModel::addCommand( const QModelIndex& menuIndex, const pCommand& command )
+{
+	if ( !menuIndex.isValid() ) {
+		return QModelIndex();
+	}
+	
+	const int count = rowCount( menuIndex );
+	
+	beginInsertRows( menuIndex, count, count );
+	mCommands[ menu( menuIndex ) ] << command;
+	endInsertRows();
+	
+	return index( count, 0, menuIndex );
+}
+
+void CommandsEditorModel::swapCommand( const QModelIndex& menuIndex, int fromCommand, int toCommand )
+{
+	#warning FIX ME ASAP
+}
+
+void CommandsEditorModel::revert()
+{
+	QAbstractItemModel::revert();
+}
+
+bool CommandsEditorModel::submit()
+{
+	const bool senderIsItemSelectionModel = sender() ? sender()->inherits( "QItemSelectionModel" ) : false;
+	
+	if ( senderIsItemSelectionModel ) {
+		return false;
+	}
+	
+	emit layoutAboutToBeChanged();
+	
+	foreach ( const QString& menu, mCommands.keys() ) {
+		pCommandList& commands = mCommands[ menu ];
+		
+		for ( int i = commands.count() -1; i >= 0; i-- ) {
+			pCommand& command = commands[ i ];
+			
+			if ( command.userData().toInt() == Qt::Checked ) {
+				commands.removeAt( i );
+			}
+		}
+	}
+	
+	emit layoutChanged();
+	
+	return true;
+}
+
+void CommandsEditorModel::addCommands( const QString& menu, const pCommandList& commands )
+{
+	if ( commands.isEmpty() ) {
+		return;
+	}
+	
+	const int count = commands.count();
+	
+	beginInsertRows( index( menu ), 0, count -1 );
+	mCommands[ menu ] = commands;
+	endInsertRows();
+}
