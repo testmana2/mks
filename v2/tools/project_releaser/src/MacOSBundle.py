f18ce74ca2963e021fcd908fa3a50a293b4cc50f
pasnox <pasnox@eb2ae826-2224-0410-8894-813e3fd817a8>
2012-03-08 22:02:45 +0000

    Support to get all project dependencies.

--- v2/tools/project_releaser/src/MacOSBundle.py
+++ v2/tools/project_releaser/src/MacOSBundle.py
@@ -7,7 +7,20 @@ import Git
 import Svn
 import Wine
 
+class ProfPrinter:
+    prof = 0;
+    
+    def plus(self):
+        self.prof += 1
+    
+    def minus(self):
+        self.prof -= 1
+    
+    def show(self, string, indent = '  '):
+        print ( indent *self.prof ) +'- ' +string
+
 class MacOSBundle:
+    prof = ProfPrinter()
     suffixedBinaries = [ 'ld', 'otool' ]
     pluginsMapping = {
         'Qt3Support': [ 'accessible' ],
@@ -62,66 +75,104 @@ class MacOSBundle:
     def getDependencies(self, binary):
         binaryFilePath = os.path.basename( binary )
         command = '"%s" -LX "%s"' % ( self.bin( 'otool' ), binary )
-        ok, output = Tools.executeAndGetOutput( command )
+        ok, output = Tools.executeAndGetOutput( command, showExecInfo = False )
         items = []
-        for line in set( output.splitlines() ):
-            line = line.strip();
-            tmp = line.lower()
-            if tmp.startswith( '/usr/lib' ) or tmp.startswith( '/system/library' ):
-                continue
-            index = line.rfind( '(compatibility' )
-            filePath = line[ :index ].strip()
-            if binaryFilePath != os.path.basename( filePath ):
-                items.append( filePath )
+        if ok:
+            for line in set( output.splitlines() ):
+                line = line.strip();
+                tmp = line.lower()
+                # system frameworks are skipped
+                if tmp.startswith( '/usr/lib' ) or tmp.startswith( '/system/library' ):
+                    continue
+                index = line.rfind( '(compatibility' )
+                filePath = line[ :index ].strip()
+                # make dependency only if file base name are not equals
+                if binaryFilePath != os.path.basename( filePath ):
+                    items.append( filePath )
+        #print items
+        #print output
         return items
     
-    def copyDependencies(self, binary, target):
-        if not Tools.createDirectory( target ):
+    def relinkDependency(self, binaryFilePath, sourceFilePath, targetFilePath):
+        self.prof.plus()
+        self.prof.show( 'Set id for %s' % ( targetFilePath ) )
+        self.prof.show( 'Set change for %s' % ( binaryFilePath ) )
+        installTool = self.bin( 'install_name_tool' )
+        frameworkName = os.path.basename( sourceFilePath )
+        idCommand = '"%s" -id "@executable_path/../Frameworks/%s" "%s"' % ( installTool, frameworkName, targetFilePath )
+        changeCommand = '"%s" -change "%s" "@executable_path/../Frameworks/%s" "%s"' % ( installTool, sourceFilePath, frameworkName, targetFilePath )
+        Tools.execute( idCommand, showError = False, showExecInfo = False )
+        Tools.execute( changeCommand, showError = False, showExecInfo = False )
+        self.prof.minus()
+        return True
+    
+    def copyDependency(self, binaryFilePath, sourceFilePath, targetPath):
+        targetFilePath = '%s/%s' % ( targetPath, os.path.basename( sourceFilePath[ 0 ] ) )
+        self.prof.plus()
+        self.prof.show( 'Copying dependency %s' % ( sourceFilePath[ 0 ] ) )
+        if not os.path.exists( targetFilePath ) and not Tools.copy( sourceFilePath[ 0 ], targetPath ):
+            self.prof.minus()
             return False
-        frameworks = self.getDependencies( binary )
-        
-        #print 'Frameworks for %s' % ( binary )
-        #print frameworks
-        
+        result = self.relinkDependency( binaryFilePath, sourceFilePath[ 1 ], targetFilePath );
+        self.prof.minus()
+        return result
+    
+    def copyDependencies(self, binaryFilePath, frameworksPath):
+        if not Tools.createDirectory( frameworksPath ):
+            return False
+        self.prof.plus()
+        self.prof.show( 'Copying dependencies for %s' % ( binaryFilePath ) )
+        frameworks = self.getDependencies( binaryFilePath )
+        if not frameworks:
+            self.prof.plus()
+            self.prof.show( '(no dependencies)' )
+            self.prof.minus()
+        # copy frameworks
         for framework in frameworks:
-            if framework == os.path.basename( binary ):
-                continue
+            localFramework = framework
+            # rebuilt framework path if needed (crossbuild)
             if not os.path.exists( framework ) and ( 'Qt' in framework or 'phonon' in framework ):
                 frameworkName = os.path.basename( framework )
                 index = framework.find( '.framework' )
-                framework = '%s/lib/%s%s' % ( self.qt.qt.macos, frameworkName, framework[ index: ] )
-            if not Tools.copy( framework, target ):
+                localFramework = '%s/lib/%s%s' % ( self.qt.qt.macos, frameworkName, framework[ index: ] )
+            # copy dependency
+            if not self.copyDependency( binaryFilePath, [ localFramework, framework ], frameworksPath ):
+                self.prof.minus()
                 return False
+            self.prof.plus()
+            # copy dependencies of the dependency
+            targetFilePath = '%s/%s' % ( frameworksPath, os.path.basename( localFramework ) )
+            if not self.copyDependencies( targetFilePath, frameworksPath ):
+                self.prof.minus()
+                self.prof.minus()
+                return False
+            self.prof.minus()
+        self.prof.minus()
         return True
     
     def deployPlugins(self):
-        frameworkFilePath = self.frameworksFilePath()
-        frameworks = os.listdir( frameworkFilePath )
-        # copy plugins
-        for framework in frameworks:
-            for plugin in MacOSBundle.pluginsMapping[ framework ]:
-                pluginsSource = self.qt.pluginsFilePath( plugin, 'macos' )
-                if os.path.exists( pluginsSource ):
-                    for file in os.listdir( pluginsSource ):
+        frameworksName = os.listdir( self.frameworksFilePath() )
+        # copy frameworks plugins
+        for frameworkName in frameworksName:
+            for plugin in MacOSBundle.pluginsMapping[ frameworkName ]:
+                pluginsPath = self.qt.pluginsFilePath( plugin, 'macos' )
+                if os.path.exists( pluginsPath ):
+                    for file in os.listdir( pluginsPath ):
                         # handle debug / release plugins according to framework name
-                        if ( '_debug' in file and '_debug' in framework ) or ( not '_debug' in file and not '_debug' in framework ):
-                            file = '%s/%s' % ( pluginsSource, file )
+                        if ( '_debug' in file and '_debug' in frameworkName ) or ( not '_debug' in file and not '_debug' in frameworkName ):
+                            file = '%s/%s' % ( pluginsPath, file )
                             if not Tools.copy( file, self.qtPluginsFilePath( plugin ) ):
                                 return False
         # copy plugins frameworks
-        for framework in Tools.getFilesList( self.qtPluginsFilePath(), recursive = True ):
-            if not self.copyDependencies( framework, frameworkFilePath ):
+        for frameworkFilePath in Tools.getFilesList( self.qtPluginsFilePath(), recursive = True ):
+            if not self.copyDependencies( frameworkFilePath, self.frameworksFilePath() ):
                 return False
         return True
     
     def deploy(self):
-        frameworkFilePath = self.frameworksFilePath()
         # copy binary dependencies
-        if not self.copyDependencies( self.binaryFilePath(), frameworkFilePath ):
+        if not self.copyDependencies( self.binaryFilePath(), self.frameworksFilePath() ):
             return False
-        # copy frameworks dependencies
-        for framework in Tools.getFilesList( frameworkFilePath ):
-            if not self.copyDependencies( framework, frameworkFilePath ):
-                return False
         # copy plugins
         return self.deployPlugins()
+        return True
